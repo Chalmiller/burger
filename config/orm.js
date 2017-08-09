01597d49b49fc5d6c88dc0d48d456b2117ff3831
Christopher Miller <christopher@transcriptic.com>
2017-08-09 10:26:33 -0700

    scripting for orm and model code

---
+++ config/orm.js
@@ -0,0 +1,27 @@
+var connection = require("../config/connection.js");
+
+var orm = {
+  selectWhere: function(table, cb) {
+    var queryString = "SELECT * FROM ??";
+
+    connection.query(queryString, [table], function(err, result) {
+      return result;
+    });
+  }
+  insertOne: function(table, name, cb) {
+  	var queryString = "INSERT INTO " + table + "(burger_name) VALUES " + name;
+
+  	connection.query(queryString, [table, name], function(err, result) {
+  		return result;
+  	});
+  }
+  updateOne: function(table, burger_name, devoured, cb) {
+  	var queryString = "UPDATE " + table + "SET " + devoured + "= TRUE WHERE burger_name = " + burger_name;
+
+  	connection.query(queryString, [table, burger_name, devoured], function(err, result) {
+  		return result;
+  	});
+  }
+};
+
+module.exports = orm;
---
+++ models/burger.js
@@ -0,0 +1,21 @@
+var orm = require("../config/orm.js");
+
+var burger = {
+	selectWhere: function(cb) {
+		orm.selectWhere("burgers", function(res) {
+			cb(res);
+		});
+	},
+	insertOne: function(name, cb) {
+		orm.insertOne("burgers", function(res) {
+			cb(res);
+		});
+	},
+	updateOne: function(burger_name, devoured, cb) {
+		orm.updateOne("burgers", function(res) {
+			cb(res);
+		});
+	}
+};
+
+module.exports = burgers;
