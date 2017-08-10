e84b827026fad5123d8d384b282e3e83e0c0b757
Christopher Miller <christopher@transcriptic.com>
2017-08-09 21:16:35 -0700

    Added the Controller Setup and the View Setup to the Eat the Burger App

--- db/schema.sql
+++ db/schema.sql
@@ -1,13 +1,9 @@
 DROP DATABASE IF EXISTS burgers_db;
 
+
 CREATE DATABASE burgers_db;
 
-USE burgers_db;
+ USE burgers_db;
 
-CREATE TABLE burgers (
-	`id` INTEGER(11) AUTO_INCREMENT NOT NULL,
-	`burger_name` VARCHAR(255) NOT NULL,
-	`devoured` BOOLEAN DEFAULT false,
-	`date_created` DATETIME DEFAULT GETDATE(),
-	PRIMARY KEY(`id`)
-)
+
+CREATE TABLE burgers ( `id` INTEGER(11) AUTO_INCREMENT NOT NULL, `burger_name` VARCHAR(255) NOT NULL, `devoured` BOOLEAN DEFAULT FALSE, `date_created` DATETIME DEFAULT GETDATE(), PRIMARY KEY(`id`))
